cdeb0754ed56070b281544ff1290194944519b28
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2018-12-05 21:23:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const client2 = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
 });
 
 client.on('ready', async() => {
@@ -13,33 +22,74 @@ var channel = "519931794011848717";//ايدي الروم
     },305);
 })
 
-
-
-client.on('ready', async() => {
-var server = "518486318200913920"; // ايدي السررفر
-var channel = "519931794011848717";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#daily')
-    },8.64e+7);
-})
-
-
-
-
 client.on('message', message => {
-if(message.content.startsWith('2s')) {
-if(message.author.id !== "#daily") return;
-var args = message.content.split(' ').slice(1).join(' ');
-message.channel.send(args);
-}
-});
- 
-client.on('message', message => {
-if(message.content.startsWith('2s')) {
-if(message.author.id !== "#daily") return;
-var args = message.content.split(' ').slice(1).join(' ');
-message.channel.send(args);
-}
+    if(message.content === '-dd'){
+        message.channel.send('#daily')
+    }
 });
 
-client.login(process.env.BOT_TOKEN); 
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '!cc'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-d'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-cc'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '!c'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client.on('message', message => { 
+if (message.content === 'y!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Spam  Spammingggggggggg, Spaam, Spaaaam , Farmingg  **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => { 
+if (message.content === 'y!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`** Spam Bot , Spamming, Credit Pro Bot , Credit Farming, Keep Spamminggggg,[PRIME]Cam Spaaaaaaaaaaaaaam,Prime Spaamming ssssssssssssssss **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
